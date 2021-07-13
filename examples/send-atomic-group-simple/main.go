7cd6ddc95eca6dec3c6b3cd39d6f14d4228c8b59
Marcelo Castellani <marcelo@linux.com>
2021-07-13 20:32:07 -0300

    Cleanup personal data

--- examples/send-atomic-group-simple/main.go
+++ examples/send-atomic-group-simple/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	ethereumNodeURL           = "https://goerli.infura.io/v3/171aba97e221493db75f0c9900902580"
+	ethereumNodeURL           = ""
 	sourceAccPvtKey1          = ""
 	sourceAccPvtKey2          = ""
 	auctionContractAddressHex = "0x1D5c3Dd2003118743D596D7DB7EA07de6C90fB20"
